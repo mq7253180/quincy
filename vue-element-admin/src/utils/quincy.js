c35dc107b09d013ade08504fecebc7274de82e0e
mq7253180 <mq7253180@126.com>
2024-10-01 01:34:53 +0800

    xxx

--- vue-element-admin/src/utils/quincy.js
+++ vue-element-admin/src/utils/quincy.js
@@ -1,24 +1,26 @@
 import axiosxxx from 'axios'
+import qs from 'qs'
 
 // axiosxxx.defaults.headers['x-requested-with'] = 'XMLHttpRequest';
 
 const service = axiosxxx.create({
   baseURL: process.env.VUE_APP_BASE_API,
-  withCredentials: true,
   timeout: 50000,
   headers: {
     'x-requested-with': 'XMLHttpRequest'
   }
 })
 
-export function ajax(params, t) {
+export function ajax(params, t, after) {
   let handle = params.handle;
   delete params.handle;
+  if (params.data!=null)
+    params.data = qs.stringify(params.data);
   service(params).then(response => {
-    if(response.status==200) {
+    if (response.status==200) {
       let status = response.data.status;
       let data = response.data.data;
-      switch(status) {
+      switch (status) {
         case 1: handle(data);break;
         case 0: {
               //alert('跳登录页')
@@ -30,8 +32,12 @@ export function ajax(params, t) {
     } else {//Not 200 except 404、500、502、503 etc.
       alert(response.status+': '+response.statusText);
     }
+    if (after!=null)
+      after()
   }).catch(error => {
     console.log(error);
     alert('请检查：\r\n前端：URL、传参；\r\n后端：404、500、502、503\r\n'+JSON.stringify(error));
+    if (after!=null)
+      after()
   });
 }
