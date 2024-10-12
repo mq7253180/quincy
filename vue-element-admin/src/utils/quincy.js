39c7a78b7ef36af1ee49b62620e9a28e379b73ad
mq7253180 <mq7253180@126.com>
2024-10-12 10:59:27 +0800

    xxx

--- vue-element-admin/src/utils/quincy.js
+++ vue-element-admin/src/utils/quincy.js
@@ -13,34 +13,31 @@ const service = axiosxxx.create({
 })
 
 export function ajax(params, t, after) {
-  const handle = params.handle;
-  delete params.handle;
-  if (params.data!=null) {
+  const handle = params.handle
+  delete params.handle
+  if (params.data != null) {
     params.data = qs.stringify(params.data)
   }
   service(params).then(response => {
-    if (response.status==200) {
-      let status = response.data.status;
-      let data = response.data.data;
+    if (response.status === 200) {
+      const status = response.data.status
+      const data = response.data.data
       switch (status) {
-        case 1: handle(data);break;
-        case 0: {
-              // alert('跳登录页')
-              t.$router.push(`/login?redirect=${t.$route.fullPath}`)
-          }break;
-        default: alert('弹层(红叉)：'+response.data.msg);break;
+        case 1: handle(data); break
+        case 0: t.$router.push(`/login?redirect=${t.$route.fullPath}`); break
+        default: alert('弹层(红叉)：' + response.data.msg); break
       }
-    } else {// Not 200 except 404、500、502、503 etc.
-      alert(response.status+': '+response.statusText);
+    } else { // Not 200 except 404、500、502、503 etc.
+      alert(response.status + ': ' + response.statusText)
     }
-    if (after!=null) {
+    if (after != null) {
       after()
     }
   }).catch(error => {
-    console.log(error);
-    alert('请检查：\r\n前端：URL、传参；\r\n后端：404、500、502、503\r\n'+JSON.stringify(error));
-    if (after!=null) {
+    console.log(error)
+    alert('请检查：\r\n前端：URL、传参；\r\n后端：404、500、502、503\r\n' + JSON.stringify(error))
+    if (after != null) {
       after()
     }
-  });
+  })
 }
