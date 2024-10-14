dd5dfbc11b3f5880e4b56a16f47e37d86d10372e
mq7253180 <mq7253180@126.com>
2024-10-14 18:37:27 +0800

    xxx

--- vue-element-admin/src/layout/components/Navbar.vue
+++ vue-element-admin/src/layout/components/Navbar.vue
@@ -73,20 +73,6 @@ export default {
       'device'
     ])
   },
-  created() {
-    if (sessionStorage.getItem('store')) {
-      this.$store.replaceState(
-        Object.assign(
-          {},
-          this.$store.state,
-          JSON.parse(sessionStorage.getItem('store'))
-        )
-      )
-    }
-    window.addEventListener('beforeunload', () => {
-      sessionStorage.setItem('store', JSON.stringify(this.$store.state))
-    })
-  },
   methods: {
     toggleSideBar() {
       this.$store.dispatch('app/toggleSideBar')
