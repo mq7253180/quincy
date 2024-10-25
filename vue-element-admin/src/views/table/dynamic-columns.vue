b767c0f86f213c98d5f6c9beb59f7489cae6e75d
mq7253180 <mq7253180@126.com>
2024-10-26 01:51:30 +0800

    xxx

--- vue-element-admin/src/views/table/dynamic-columns.vue
+++ vue-element-admin/src/views/table/dynamic-columns.vue
@@ -1,19 +1,61 @@
 <template>
   <div class="app-container">
-    <table v-loading="listLoading" border fit highlight-current-row style="width: 100%;" class="el-table__header" @sort-change="sortChange">
-      <tr>
-        <td width="150px" align="center">id</td>
-        <td width="150px" align="center">eee</td>
-        <td width="150px" align="center">fff</td>
-        <td v-for="dynamicFieldName in dynamicFieldNames" :key="dynamicFieldName.index" width="150px" align="center">{{ dynamicFieldName }}</td>
-      </tr>
-      <tr v-for="row in list" :key="row.id">
-        <td>{{ row.id }}</td>
-        <td>{{ row.eee }}</td>
-        <td>{{ row.fff }}</td>
-        <td v-for="dynamicField in row.dynamicFields" :key="dynamicField.index">{{ dynamicField.value }}</td>
-      </tr>
-    </table>
+    <div class="el-table el-table--fit el-table--border el-table--enable-row-hover el-table--enable-row-transition el-table--medium" style="width: 100%;">
+      <div class="el-table__header-wrapper">
+        <table v-loading="listLoading" cellspacing="0" cellpadding="0" border="0" class="el-table__header" style="width: 1170px;">
+          <thead class="">
+            <tr>
+              <th colspan="1" rowspan="1" class="el-table_3_column_12 is-center ascending is-leaf is-sortable" style="cursor: col-resize;">
+                <div class="cell">
+                  ID
+                  <span class="caret-wrapper">
+                    <i class="sort-caret ascending"></i>
+                    <i class="sort-caret descending"></i>
+                  </span>
+                </div>
+              </th>
+              <th colspan="1" rowspan="1" class="el-table_3_column_13 is-center is-leaf">
+                <div class="cell">EEE</div>
+              </th>
+              <th colspan="1" rowspan="1" class="el-table_3_column_14 is-center is-leaf">
+                <div class="cell">FFF</div>
+              </th>
+              <th v-for="dynamicFieldName in dynamicFieldNames" :key="dynamicFieldName.index" colspan="1" rowspan="1" class="el-table_3_column_15 is-center is-leaf">
+                <div class="cell">{{ dynamicFieldName }}</div>
+              </th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+      <div class="el-table__body-wrapper is-scrolling-none">
+        <table cellspacing="0" cellpadding="0" border="0" class="el-table__body" style="width: 1170px;">
+          <tbody>
+            <tr v-for="row in list" :key="row.id" class="el-table__row">
+              <td rowspan="1" colspan="1" class="el-table_3_column_12 is-center ascending">
+                <div class="cell">
+                  <span>{{ row.id }}</span>
+                </div>
+              </td>
+              <td rowspan="1" colspan="1" class="el-table_3_column_13">
+                <div class="cell">
+                  <span class="link-type">{{ row.eee }}</span>
+                </div>
+              </td>
+              <td rowspan="1" colspan="1" class="el-table_3_column_14">
+                <div class="cell">
+                  <span>{{ row.fff }}</span>
+                </div>
+              </td>
+              <td v-for="dynamicField in row.dynamicFields" :key="dynamicField.index" rowspan="1" colspan="1" class="el-table_3_column_15 is-right">
+                <div class="cell">
+                  <span>{{ dynamicField.value }}</span>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,7 +83,6 @@ export default {
         responseType: 'JSON',
         // data: {},
         handle: (data) => {
-          // alert(JSON.stringify(data))
           this.list = data.result
           this.dynamicFieldNames = data.dynamicFieldNames
           this.listLoading = false
